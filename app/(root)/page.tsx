7198582561d9c4936e5082458f808b5ed9bceb9b
ahmedtariq08 <ahmedtariq92331@gmail.com>
2024-05-20 22:44:45 +0800

    sidebars 1:22

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Ahmed" };
+  const loggedIn = {
+    firstName: "Ahmed",
+    lastName: "Tariq",
+    email: "ahmedtariq92331@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -14,13 +19,15 @@ const Home = () => {
             user={loggedIn.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={500.43}
+          />
         </header>
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={500.43}
-        />
+        recent transactions
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
